b5398549ef4957814bb92502ce4506d458438154
Faznug_Space <153061199+FaishalFaiz@users.noreply.github.com>
2026-02-06 20:44:18 +0700

    add icon

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
+    <link rel="icon" href="webicon.svg" sizes="32x32" />
 
     <style>
         body {
